b828bedb42977ead7931ffb6ae344ff71bbc6614
Joao Viana <jguilherme_viana@hotmail.com>
2018-03-14 11:53:39 +0100

    change my_profile to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  get "/my_profile", to: "pages#my_profile", as: :my_profile
+  get "/dashboard", to: "pages#dashboard", as: :dashboard
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
