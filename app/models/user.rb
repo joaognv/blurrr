fea521673279aece4924113d228c5be50aec394c
Joao Viana <jguilherme_viana@hotmail.com>
2018-03-14 13:05:12 +0100

    added chats to the dashboard page

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
   has_many :messages
   has_many :user_answers
   has_many :answers, through: :user_answers
-  has_many :matches, through: :user_matches
   has_many :user_matches
+  has_many :matches, through: :user_matches
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
