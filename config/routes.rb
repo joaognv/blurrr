31c95d0acf899b364d11b7157cd7ff35a4027481
Joao Viana <jguilherme_viana@hotmail.com>
2018-03-19 23:30:23 +0100

    merging attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,22 @@
 Rails.application.routes.draw do
   get 'questions/show'
   get 'matches/show'
+
   root to: 'pages#home'
 
   devise_for :users, controllers: { registrations: "registrations" }
 
   get "/dashboard", to: "pages#dashboard", as: :dashboard
+
+
   resources :profiles, only: [:show, :new, :create, :edit, :update]
-  resources :matches, only: [:show, :new]
+  resources :matches, only: [:show, :new] do
+    resources :messages, only: [:index]
+  end
   resources :messages, only:[:create]
   resources :questions, only: [:index]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
