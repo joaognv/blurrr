a06ef6c4a56f9f23dd4dc02a6334f0241e4c2f88
Joao Viana <jguilherme_viana@hotmail.com>
2018-03-20 18:16:31 +0100

    after migrating and conflict solving

--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,6 @@ ActiveRecord::Schema.define(version: 20180315113843) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo_url"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
