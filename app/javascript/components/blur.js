0ea61e531490dde8d9d61e7f3944c1ac020f08fb
Michael Arizola <mdarizola@gmail.com>
2018-03-21 15:48:40 +0100

    unblur of avatar javascript

--- app/javascript/components/blur.js
+++ app/javascript/components/blur.js
@@ -1,5 +1,16 @@
 function blur() {
-
-
-
+  function updateBlur() {
+    let bubbles = document.getElementsByClassName('chat-bubble');
+    let unblur = Math.round(bubbles.length);
+            console.log(unblur)
+            // console.log(bubbles)
+      if (unblur > 7) {
+        unblur = 5;
+      }
+    let avatar = document.querySelector('.avatar');
+    avatar.style.filter = `blur(${5 - unblur}px)`;
+  }
+  setInterval(updateBlur, 1000);
 }
+
+export {blur};
